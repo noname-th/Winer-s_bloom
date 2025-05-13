60beecd7e7731c8e84c291cec191ab99f0b84277
noname-th <wrish502@gmail.com>
2025-05-13 10:53:24 +0700

    init Winter's bloom project.

---
+++ script/res/characterData.gd
@@ -0,0 +1,9 @@
+# script/res/CharacterData.gd
+extends Resource
+class_name CharacterData
+
+@export var character_name: String
+@export var gender: String
+@export var base_friendship: int
+@export var dialogue_path: String
+@export var personality_tags: Array[String]
